eee4bd816810ee17176b0e90a70530b30cc2a09d
george39 <george35@utp.edu.co>
2017-10-08 13:39:10 -0500

    version incial

--- src/distribuidor/views.py
+++ src/distribuidor/views.py
@@ -4,6 +4,6 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from django.http import HttpResponse
 def index(reguest):
-	return HttpResponse('pagina de distribuidor')
+	return HttpResponse('pagina de proveedor')
 # Create your views here.
 

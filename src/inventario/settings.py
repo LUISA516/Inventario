4c2517e0424e4fa0bbb30087251ae947e89380b8
george39 <george35@utp.edu.co>
2017-10-08 19:09:23 -0500

    agregar css y js

--- src/inventario/settings.py
+++ src/inventario/settings.py
@@ -124,3 +124,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#para buscar archivos css y js
+SATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
